ac3c1ff23589cfb450b465ac706f267c98afb0c8
Shun Nishimura <n.shun1110@gmail.com>
2019-02-18 19:20:07 +0900

    Contest: abs/abc086_a

---
+++ abs/abc086_a/Main.cpp
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abs/tasks/abc086_a
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b;
+
+  cin >> a >> b;
+
+  if (a % 2 == 0 || b % 2 == 0)
+    cout << "Even" << endl;
+  else
+    cout << "Odd" << endl;
+
+  return 0;
+}
