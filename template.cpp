f1c59c6cab046e0bbed4b23d597bcacefbc6b9b9
Shun Nishimura <n.shun1110@gmail.com>
2019-02-19 17:19:20 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  /* Magic word */
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  /* ---------- */
+
+  return 0;
+}
