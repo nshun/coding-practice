829648dac1b54641529a344b86cf6001b8a15d53
Shun Nishimura <n.shun1110@gmail.com>
2019-02-19 18:16:13 +0900

    Contest: abc046_a

---
+++ atcoder/abc046_a/Main.cpp
@@ -0,0 +1,26 @@
+// https://atcoder.jp/contests/
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+  /* Magic word */
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  /* ---------- */
+
+  int a, b, c;
+  set<int> s;
+
+  cin >> a >> b >> c;
+
+  s.insert(a);
+  s.insert(b);
+  s.insert(c);
+
+  cout << s.size() << "\n";
+
+  return 0;
+}
