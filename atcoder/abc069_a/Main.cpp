b536d17bd4218565c7b55fd3537937f4e8f81c6b
Shun Nishimura <n.shun1110@gmail.com>
2019-02-20 01:35:24 +0900

    Contest: abc069_a

---
+++ atcoder/abc069_a/Main.cpp
@@ -0,0 +1,20 @@
+// https://atcoder.jp/contests/abc069/tasks/abc069_a
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  /* Magic word */
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  /* ---------- */
+
+  int n, m;
+
+  cin >> n >> m;
+
+  cout << (n - 1) * (m - 1) << '\n';
+
+  return 0;
+}
