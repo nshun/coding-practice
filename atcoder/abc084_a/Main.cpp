c21d266afa7aef251532b035461433f1dde5bde0
Shun Nishimura <n.shun1110@gmail.com>
2019-02-21 23:01:53 +0900

    Contest: abc084_a

---
+++ atcoder/abc084_a/Main.cpp
@@ -0,0 +1,20 @@
+// https://atcoder.jp/contests/abc084/tasks/abc084_a
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  /* Magic word */
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  /* ---------- */
+
+  int m;
+
+  cin >> m;
+
+  cout << (48 - m) << '\n';
+
+  return 0;
+}
