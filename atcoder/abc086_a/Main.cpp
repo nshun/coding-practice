b08882da1cfe699a80c2b22d904db97b9d34c6df
Shun Nishimura <n.shun1110@gmail.com>
2019-02-21 23:02:03 +0900

    Contest: abc086_a

--- atcoder/abc086_a/Main.cpp
+++ atcoder/abc086_a/Main.cpp
@@ -1,17 +1,23 @@
-// https://atcoder.jp/contests/abs/tasks/abc086_a
+// https://atcoder.jp/contests/abc086/tasks/abc086_a
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+  /* Magic word */
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  /* ---------- */
+
   int a, b;
 
   cin >> a >> b;
 
   if (a % 2 == 0 || b % 2 == 0)
-    cout << "Even" << endl;
+    cout << "Even" << '\n';
   else
-    cout << "Odd" << endl;
+    cout << "Odd" << '\n';
 
   return 0;
 }
