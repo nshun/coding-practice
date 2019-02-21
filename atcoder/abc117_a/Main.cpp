bcde7c569d6f8af639f8feb39923dbc42511cac0
Shun Nishimura <n.shun1110@gmail.com>
2019-02-22 00:56:26 +0900

    Contest: abc117_a

---
+++ atcoder/abc117_a/Main.cpp
@@ -0,0 +1,20 @@
+// https://beta.atcoder.jp/contests/abc117/tasks/abc117_a
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  /* Magic word */
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  /* ---------- */
+
+  double t, x;
+
+  cin >> t >> x;
+
+  cout << t / x << '\n';
+
+  return 0;
+}
