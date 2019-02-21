818e05745fc05e94f6c0a053cd7ddfaa3bc51de0
Shun Nishimura <n.shun1110@gmail.com>
2019-02-22 00:25:10 +0900

    Contest: abc106_a

---
+++ atcoder/abc106_a/Main.cpp
@@ -0,0 +1,20 @@
+// https://atcoder.jp/contests/abc106/tasks/abc106_a
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  /* Magic word */
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  /* ---------- */
+
+  int a, b;
+
+  cin >> a >> b;
+
+  cout << (a - 1) * (b - 1) << '\n';
+
+  return 0;
+}
