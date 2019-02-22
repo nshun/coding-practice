51966698af11ee8e111d70d96cc0951aa70a48f7
Shun Nishimura <n.shun1110@gmail.com>
2019-02-22 19:56:15 +0900

    Contest: abc056_b

---
+++ atcoder/abc056_b/Main.cpp
@@ -0,0 +1,26 @@
+// https://atcoder.jp/contests/abc056/tasks/abc056_b
+
+#include <bits/stdc++.h>
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+  /* Magic word */
+  // cin.tie(0);
+  // ios::sync_with_stdio(false);
+  /* ---------- */
+
+  int w, a, b;
+
+  cin >> w >> a >> b;
+
+  cout << max(0, max(a, b) - min(a, b) - w) << '\n';
+
+  return 0;
+}
