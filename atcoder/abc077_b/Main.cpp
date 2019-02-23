884c0eb8bc447ef9cd68db4980b50f81550b99ed
Shun Nishimura <n.shun1110@gmail.com>
2019-02-23 12:47:36 +0900

    Contest: abc077_b

---
+++ atcoder/abc077_b/Main.cpp
@@ -0,0 +1,26 @@
+// https://atcoder.jp/contests/abc077/tasks/abc077_b
+
+#include <bits/stdc++.h>
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+  /* Magic word */
+  // cin.tie(0);
+  // ios::sync_with_stdio(false);
+  /* ---------- */
+
+  ll n;
+
+  cin >> n;
+
+  cout << (ll)pow(floor(sqrt(n)), 2) << '\n';
+
+  return 0;
+}
