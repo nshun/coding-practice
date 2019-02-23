d32ed5e05a402bb87f16e4b223a1f43628dd3d8e
Shun Nishimura <n.shun1110@gmail.com>
2019-02-23 23:14:00 +0900

    Contest: abc111_b

---
+++ atcoder/abc111_b/Main.cpp
@@ -0,0 +1,29 @@
+// https://atcoder.jp/contests/abc111/tasks/abc111_b
+
+#include <bits/stdc++.h>
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+  /* Magic word */
+  // cin.tie(0);
+  // ios::sync_with_stdio(false);
+  /* ---------- */
+
+  int n, r = 0;
+
+  cin >> n;
+
+  while (r < n)
+    r += 111;
+
+  cout << r << '\n';
+
+  return 0;
+}
