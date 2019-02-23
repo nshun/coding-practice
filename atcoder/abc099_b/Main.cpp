b6d2bb75e430eebfcf34bc3eaaa4131e83b3bb75
Shun Nishimura <n.shun1110@gmail.com>
2019-02-23 18:36:42 +0900

    Contest: abc099_b

---
+++ atcoder/abc099_b/Main.cpp
@@ -0,0 +1,30 @@
+// https://atcoder.jp/contests/abc099/tasks/abc099_b
+
+#include <bits/stdc++.h>
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+  /* Magic word */
+  // cin.tie(0);
+  // ios::sync_with_stdio(false);
+  /* ---------- */
+
+  int a, b;
+
+  cin >> a >> b;
+
+  int i = b - a - 1;
+
+  int x = i * (i + 1) / 2 - a;
+
+  cout << x << '\n';
+
+  return 0;
+}
