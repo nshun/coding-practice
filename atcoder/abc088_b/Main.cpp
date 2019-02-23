57d8202d4f93e70126381afd62e9ea6141819b93
Shun Nishimura <n.shun1110@gmail.com>
2019-02-23 16:37:29 +0900

    Contest: abc088_b

--- atcoder/abc088_b/Main.cpp
+++ atcoder/abc088_b/Main.cpp
@@ -1,16 +1,22 @@
-// https://atcoder.jp/contests/abs/tasks/abc088_b
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// https://atcoder.jp/contests/abc088/tasks/abc088_b
+
+#include <bits/stdc++.h>
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
 using namespace std;
+typedef long long ll;
 
 int main()
 {
-  int n,
-      ai,
-      aT = 0,
-      bT = 0;
+  /* Magic word */
+  // cin.tie(0);
+  // ios::sync_with_stdio(false);
+  /* ---------- */
 
+  int n, ai, aT = 0, bT = 0;
   vector<int> a;
 
   cin >> n;
@@ -32,7 +38,7 @@ int main()
     turn_a = !turn_a;
   }
 
-  cout << (aT - bT) << endl;
+  cout << (aT - bT) << '\n';
 
   return 0;
 }
