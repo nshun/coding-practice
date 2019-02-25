7c4a3b3772a70dba5fddaf34fc80b1065d5b28a3
Shun Nishimura <n.shun1110@gmail.com>
2019-02-25 21:41:06 +0900

    Contest: abc051_c

---
+++ atcoder/abc051_c/Main.cpp
@@ -0,0 +1,47 @@
+// https://atcoder.jp/contests/
+
+#include <bits/stdc++.h>
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+ll sx, sy, tx, ty, dx, dy;
+string p;
+
+void move(string &p, char d, int c)
+{
+  REP(i, c)
+  {
+    p += d;
+  }
+}
+
+int main()
+{
+  p = "";
+  cin >> sx >> sy >> tx >> ty;
+
+  dx = tx - sx;
+  dy = ty - sy;
+  move(p, 'U', dy);
+  move(p, 'R', dx);
+  move(p, 'D', dy);
+  move(p, 'L', dx);
+
+  p += 'L';
+  move(p, 'U', dy + 1);
+  move(p, 'R', dx + 1);
+  p += "DR";
+  move(p, 'D', dy + 1);
+  move(p, 'L', dx + 1);
+  p += 'U';
+
+  cout << p << '\n';
+
+  return 0;
+}
