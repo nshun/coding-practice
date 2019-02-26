01292ab83a4c0519fe0ce5d8871ec52ac15a8b2c
Shun Nishimura <n.shun1110@gmail.com>
2019-02-27 01:06:27 +0900

    Contest: abc076_c

---
+++ atcoder/abc076_c/Main.cpp
@@ -0,0 +1,58 @@
+// https://atcoder.jp/contests/abc076/tasks/abc076_c
+
+#include <bits/stdc++.h>
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+
+int p, s, k, l;
+string S, Sd, T;
+
+int main()
+{
+  cin >> Sd >> T;
+
+  s = p = 0;
+  k = T.length();
+  l = Sd.length();
+  reverse(ALL(T));
+  reverse(ALL(Sd));
+  REP(i, l)
+  {
+    if (Sd[i] == T[p] || Sd[i] == '?')
+    {
+      if (p == 0)
+        s = i;
+      p++;
+      if (p == k)
+        break;
+    }
+    else if (p != 0)
+    {
+      i -= p;
+      p = 0;
+    }
+  }
+
+  S = "a";
+  if (p == k)
+  {
+    REP(i, l)
+    {
+      S += s <= i && i < s + k ? T[i - s] : Sd[i] == '?' ? 'a' : Sd[i];
+    }
+    S = S.substr(1);
+    reverse(ALL(S));
+  }
+  else
+    S = "UNRESTORABLE";
+
+  cout << S << '\n';
+
+  return 0;
+}
