adee1c70269aac6cf485e13ef8dbe8d433b82e7b
Shun Nishimura <n.shun1110@gmail.com>
2019-03-04 01:21:13 +0900

    Contest: arc094_a

---
+++ atcoder/arc094_a/Main.cpp
@@ -0,0 +1,28 @@
+// https://atcoder.jp/contests/
+
+#include <bits/stdc++.h>
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+
+int x[3];
+
+int main()
+{
+  cin >> x[0] >> x[1] >> x[2];
+
+  sort(x, x + 3);
+
+  int bc = x[2] - x[1],
+      ab = x[1] - x[0],
+      r = ab % 2 ? bc + ab / 2 + 2 : bc + ab / 2;
+
+  cout << r << '\n';
+
+  return 0;
+}
