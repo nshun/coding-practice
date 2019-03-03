c036ac1d9848b1e485913362a538138568a05361
Shun Nishimura <n.shun1110@gmail.com>
2019-03-04 00:52:14 +0900

    Contest: abc120_a

---
+++ atcoder/abc120_a/Main.cpp
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc120/tasks/abc120_a
+
+#include <bits/stdc++.h>
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+
+int main()
+{
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  cout << min(B / A, C) << '\n';
+
+  return 0;
+}
