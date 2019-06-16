9bd8916a0d84f4d46a46dc098aa081b64e0654ec
Shun Nishimura <n.shun1110@gmail.com>
2019-06-17 00:41:19 +0900

    Contest: abc130

---
+++ atcoder/abc130_a/Main.cpp
@@ -0,0 +1,29 @@
+// https://atcoder.jp/contests/abc130/tasks/abc130_a
+
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define FORR(i, m, n) for (int(i) = (n); (i) >= (m); i--)
+#define rep(...) _overload3(__VA_ARGS__, FOR, REP, )(__VA_ARGS__)
+#define repr(...) _overload3(__VA_ARGS__, FORR, REPR, )(__VA_ARGS__)
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+
+int main()
+{
+  int X, A;
+  cin >> X >> A;
+
+  cout << (X < A ? 0 : 10) << '\n';
+  return 0;
+}
---
+++ atcoder/abc130_b/Main.cpp
@@ -0,0 +1,38 @@
+// https://atcoder.jp/contests/abc130/tasks/abc130_b
+
+#include <bits/stdc++.h>
+#define _overload3(_1, _2, _3, name, ...) name
+#define REP(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define REPR(i, n) for (int(i) = (n); (i) >= 0; (i)--)
+#define FOR(i, m, n) for (int(i) = (m); (i) < (n); i++)
+#define FORR(i, m, n) for (int(i) = (n); (i) >= (m); i--)
+#define rep(...) _overload3(__VA_ARGS__, FOR, REP, )(__VA_ARGS__)
+#define repr(...) _overload3(__VA_ARGS__, FORR, REPR, )(__VA_ARGS__)
+#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
+#define INF 1e9
+#define ALL(v) (v).begin(), (v).end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+
+int main()
+{
+  int N, X, l, d = 0, cnt = 1;
+  cin >> N >> X;
+
+  REP(i, N)
+  {
+    cin >> l;
+    d += l;
+    if (d <= X)
+      cnt++;
+  }
+
+  cout << cnt << '\n';
+
+  return 0;
+}
